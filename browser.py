e2741e8912178523c13fd6f47a63fcd7d0e6152d
Jack <jetneotw@gmail.com>
2023-02-20 14:35:09 +0800

    Initial commit

---
+++ browser.py
@@ -0,0 +1,32 @@
+# Use the Request library
+import requests
+
+# Set the target webpage
+url = "http://www.wikipedia.org"
+webpage = requests.get(url)
+
+# This will get the full webpage in text
+print(webpage.text)
+
+# Get and print the status code
+print("Status code:")
+print("\t *", webpage.status_code)
+
+# Get the headers
+h = requests.head(url)
+print("Header:")
+print("**********")
+# To print line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
+# Modify the headers user-agent
+headers = {'User-Agent': 'Iphone 14'}
+
+# Test it on an external site
+url2 = 'http://httpbin.org/headers'
+request_header = requests.get(url2, headers=headers)
+print(request_header.text)
+
+
